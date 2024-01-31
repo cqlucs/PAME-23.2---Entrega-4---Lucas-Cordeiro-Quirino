1231a2ed01888db7150e8da601dcd54ce390f13c
cqlucs <cqlucs@gmail.com>
2024-01-31 17:08:49 -0300

    oitava commit, infos do login sao armazenadas apropriadamente

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useState } from 'react';
 import { Body, Container, LoginButton, Pages, StyledLink } from "./styles";
 import Header from "../../Components/Header";
 import Input from "../../Components/Input";
 
 function Login() {
+    // Para receber dados de login
+    const [username, setusername] = useState("");
+    const [password, setpassword] = useState("");
+
     return (
         <Pages>
             <Header isntLogin={false}></Header>
@@ -11,9 +16,9 @@ function Login() {
                 <Container>
                     <h1>Login</h1>
 
-                    <Input placeholder="Nome..." type="text"></Input>
+                    <Input placeholder="Nome..." type="text" setvariavel={setusername} value={username}></Input>
                     <p>Esqueci minha senha!</p>
-                    <Input placeholder="Senha..." type="password"></Input>
+                    <Input placeholder="Senha..." type="password" setvariavel={setpassword} value={password}></Input>
                     <StyledLink to="/Estoque">
                         <LoginButton>
                             <span>Login</span>
