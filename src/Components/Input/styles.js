c81c6d2efae35ddaf9b3020c8ae753512c63fef4
cqlucs <cqlucs@gmail.com>
2024-02-01 19:38:41 -0300

    13 commit, fix visual alterado

--- src/Components/Input/styles.js
+++ src/Components/Input/styles.js
@@ -5,7 +5,7 @@ export const InputField = styled.input`
     height: 5vh;
     padding: 0;
     background-color: #9b4c37;
-    border: solid #c1a68d 3px;
+    border: solid #cec9c5 3px;
     border-radius: 5px;
     font-size: 16px;
     margin-bottom: 30px;
